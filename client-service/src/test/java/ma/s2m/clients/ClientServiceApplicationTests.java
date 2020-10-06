132ac0e24f13c543447320bcb0c108bc3161ad6a
Reda <ghalou.reda@gmail.com>
2020-10-07 00:09:48 +0100

    First Commit

---
+++ client-service/src/test/java/ma/s2m/clients/ClientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.s2m.clients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
