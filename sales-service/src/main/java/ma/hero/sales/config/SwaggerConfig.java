750ec90d4702da32fca898a956111ac98199d0d7
Thinkbook <relghallouch@s2m.ma>
2021-08-09 22:45:12 +0100

    step04/sales-service: Adding Sales Service - Initialize sales-service MS; - Develop CRUD for Sale Entity; - Develop Unit Tests for Sale Controller and Service.

---
+++ sales-service/src/main/java/ma/hero/sales/config/SwaggerConfig.java
@@ -0,0 +1,40 @@
+package ma.hero.sales.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("ma.omnishore.sales.api"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+
+                .title("Sales Management Rest APIs")
+
+                .description("This page lists all the rest apis for Sales Management App.")
+
+                .version("1.0-SNAPSHOT").contact(new Contact("Amine BOUAGGAD", "www.omnidata.ma", "abouaggad@omnidata.ma"))
+
+                .build();
+    }
+
+}
